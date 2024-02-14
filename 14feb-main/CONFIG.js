5a0d1aac59183e8561397b35a7ac845e336c6c3d
Faruk Erdem <farukomererdem2003@gmail.com>
2024-02-14 19:41:06 +0300

    <3

---
+++ 14feb-main/CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Bugün Sevgililer Günü ve sana söyleyecek önemli bir şeyim var...",
+  descriptionHeader:
+    "Bugün Sevgililer Günü ve sana olan sevgimi kelimelere dökmek imkansız. Seni çooooook seviyorum ve iyi ki varsın!<br>Hayatımda olduğun için her gün şükrediyorum. Sen bana neşe, sevgi ve mutluluk getiriyorsun. Seninle her şey daha güzel, daha anlamlı. <br>Yine Cihangirde yine güzel manzaram olur musun?",
+  buttonYes: "Oluuur <3",
+  buttonNo: "Olmaz :(",
+  titleModar: "love u <3",
+  descriptionModar:
+    "Seni seviyorum <3",
+};
